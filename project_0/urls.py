bc0c15ccacbed487c16b0bb844588d1d8a28d27f
Kostyantin Zahorodnyi <constantine2903@gmail.com>
2025-01-28 15:11:28 +0000

    refactor and delete admin

--- project_0/urls.py
+++ project_0/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('dashboard.urls')),
-    path('admin/', admin.site.urls),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
 ]
