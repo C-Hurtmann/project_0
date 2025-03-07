cc4ff82a0d289395c232ee4d930960113c56f129
Constantine Zagorodnyi <constantine2903@gmail.com>
2025-03-07 13:42:35 +0300

    finish scatter plot

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,25 +1,47 @@
 from django.shortcuts import render
 from plotly.offline import plot
+import plotly.express as px
 import plotly.graph_objects as go
+import pandas as pd 
 
 from collector.models import Transaction
-from collector.utils import to_datetime
 
 
 # Create your views here.
 def home(request):
-    transactions = Transaction.objects.all()
+    transactions = pd.DataFrame(list(Transaction.objects.order_by('unix_time').values()))
+    transactions['date'] = pd.to_datetime(transactions['unix_time'], unit='s').dt.date
 
-    x1 = [to_datetime(transaction.unix_time) for transaction in transactions]
-    y1 = [(transaction.balance // 100) for transaction in transactions]
+    date_range = pd.date_range(transactions['date'].min(), transactions['date'].max(), freq='D').date
+    start_transaction = transactions[transactions['unix_time'] == transactions['unix_time'].min()]
+    start_balance = start_transaction['balance'].iloc[0] - start_transaction['amount'].iloc[0]
+
+    result_by_date = pd.merge(
+        pd.DataFrame({'date': date_range}),
+        transactions.groupby('date')['amount'].sum().reset_index(),
+        on='date',
+        how='left'
+    )
+    result_by_date['amount'].fillna(0, inplace=True)
+    result_by_date['balance'] = result_by_date['amount'].cumsum() + start_balance
+    result_by_date['balance'] = (result_by_date['balance'] / 100).round(2)
+
+
+    x = result_by_date['date'].tolist()
+    y = result_by_date['balance'].tolist()
     
-    trace = go.Scatter(
-        x=x1,
-        y=y1,
-        mode='lines+markers',
+
+    scatter = go.Scatter(
+        x=x,
+        y=y,
+        mode='lines',
         fill='tozeroy',
-        line=dict(color='rgba(78, 115, 223, 1)', width=2),
-        marker=dict(size=6, color='rgba(78, 115, 223, 1)'),
+        fillcolor='rgba(78, 115, 223, 0.3)',
+        line=dict(
+            color='rgba(78, 115, 223, 1)',
+            width=3,
+            shape='linear'
+        ),
         hoverinfo='y',
         name='Balance'
     )
@@ -30,14 +52,19 @@ def home(request):
         ),
         xaxis=dict(
             title='Date',
-            range=[min(x1), max(x1)],
             showgrid=True,
             gridcolor='rgba(234, 236, 244, 1)',
-            zeroline=False
+            zeroline=False,
+            rangeslider=dict(
+                visible=True,
+                thickness=0.05,
+                bgcolor="lightgray",
+                borderwidth=1,
+                bordercolor="grey",
+            )
         ),
         yaxis=dict(
             title='Balance (UAH)',
-            range=[min(y1) - 10, max(y1) + 10],
             showgrid=True,
             gridcolor='rgba(234, 236, 244, 1)',
             zeroline=False
@@ -45,11 +72,10 @@ def home(request):
         paper_bgcolor='rgba(255, 255, 255, 1)',
         plot_bgcolor='rgba(255, 255, 255, 1)',
         hovermode='x unified',
-        margin=dict(l=20, r=20, t=40, b=40),
-        # autosize=True
+        margin=dict(l=20, r=20, t=40, b=40)
     )
-    fig = go.Figure(data=[trace], layout=layout)
-    plot_div = plot(fig, output_type='div', include_plotlyjs=False)
+    fig = go.Figure(data=[scatter], layout=layout)
+    plot_div = plot(fig, output_type='div')
     context = {'plot_div': plot_div}
     
     return render(request, 'dashboard/home.html', context)
